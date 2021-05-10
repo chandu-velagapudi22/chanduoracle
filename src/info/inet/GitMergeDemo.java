2efde6a6a27e865ed5df4a340c62cce8094ebfeb
chandu velagapudi <chandu.velagapudi22@gmail.com>
2021-05-10 16:16:37 +0530

    git merge first commit

---
+++ src/info/inet/GitMergeDemo.java
@@ -0,0 +1,12 @@
+package info.inet;
+
+public class GitMergeDemo {
+
+	public static void main(String[] args) {
+		System.out.println("i am here....");
+	}
+	
+	public static void m1() {
+		System.out.println("i am in m1() method");
+	}
+}
